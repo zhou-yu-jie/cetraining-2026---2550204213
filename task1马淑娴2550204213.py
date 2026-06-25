89b372d26d34cd0d23205007e81d547f6797f829
zhou-yu-jie <2796049437@qq.com>
2026-06-25 17:51:14 +0800

    Delete task1马淑娴2550204213.py

--- task1马淑娴2550204213.py
+++
@@ -1,8 +0,0 @@
-import re
-from collections import Counter
-
-def count_words_in_file(file_path):
-    with open(file_path, "r", encoding="utf-8") as f:
-        text = f.read().lower()
-    words = re.findall(r"\b\w+\b", text, flags=re.UNICODE)
-    return Counter(words)
